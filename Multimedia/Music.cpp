f639026f3d35cc1b88df01fe242dbfc4bcb9ba7b
cemkalyoncu <cemkalyoncu@gmail.com>
2012-12-26 13:16:14 +0000

    * Removed unnecessary files * Multimedia module * Structure for new Music class

---
+++ Multimedia/Music.cpp
@@ -0,0 +1,9 @@
+#include "Music.h"
+
+
+namespace gge { namespace multimedia {
+
+	int StreamingMusic::DefaultBufferSize = 4096;
+	int StreamingMusic::DefaultNumberOfBuffers = 4;
+
+}}
---
+++ Multimedia/Music.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "../Engine/Sound.h"
+
+
+namespace gge { namespace multimedia {
+
+
+	class Music {
+	public:
+		Music();
+		bool Seek(unsigned time);
+		bool IsFinished() const;
+		bool Play();
+		bool Loop();
+		bool IsLooping() const;
+		bool Pause();
+		void Release();
+		bool Acquire();
+		void SetVolume();
+		float GetVolume() const;
+	};
+
+	class StreamingMusic : public Music {
+	public:
+
+		void SetBufferSize(int size);
+		int GetBufferSize() const;
+
+		static int DefaultBufferSize;
+		static int DefaultNumberOfBuffers;
+
+	protected:
+		virtual int decode(std::vector<Byte> &buffer)=0;
+		void buffercheck();
+
+		std::vector<Byte> buffer;
+	};
+
+	class VorbisStream : public StreamingMusic {
+	public:
+		VorbisStream(const std::string filename);
+		VorbisStream(std::ifstream &stream);
+		VorbisStream();
+
+
+
+		bool Open(const std::string filename);
+		bool Use(std::ifstream &stream);
+
+
+
+		static int BufferSize;
+	};
+
+}}
+
