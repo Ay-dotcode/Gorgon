06fd6f094502d80eb427e19af5ce3e3c333cdff6
cemkalyoncu <cemkalyoncu@gmail.com>
2013-02-24 23:01:12 +0000

    * Refcounter is put into utils namespace

--- Utils/RefCounter.h
+++ Utils/RefCounter.h
@@ -31,81 +31,84 @@
 
 #define REFCOUNTER_EXIST
 
+namespace gge { namespace utils {
 
-//RefCounter requires following methods to be implemented by client
-//classes:
-// void dealloc() : called when no reference left, should deallocate
-//                  buffers, set counts to 0, etc... object is still alive 
-//                  and accessible, so control variables can be left out
-// void destroy() : destroys everything, this is the last chance of freeing
-//                  resources, nothing should be left out
-//Copy constructor should increment counter, assignment should assign target ref 
-//count to object's refcount, destructor should decrement it.
-template <class C_>
-class RefCounter {
-  template<class C2_> friend class RefCounter;
-protected:
-	RefCounter() : refcnt(new int(1)) {	
-	}
-
-	template <class C2_>
-	RefCounter(const RefCounter<C2_> &ref) : refcnt(ref.refcnt) {
-	}
-
-	RefCounter(const RefCounter &ref) : refcnt(ref.refcnt) {
-	}
-
-	void refassign(const RefCounter &ref) {
-		removeref();
-
-		if(!refcheck()) {
-			static_cast<C_*>(this)->destroy();
-			delete refcnt;
+	//RefCounter requires following methods to be implemented by client
+	//classes:
+	// void dealloc() : called when no reference left, should deallocate
+	//                  buffers, set counts to 0, etc... object is still alive 
+	//                  and accessible, so control variables can be left out
+	// void destroy() : destroys everything, this is the last chance of freeing
+	//                  resources, nothing should be left out
+	//Copy constructor should increment counter, assignment should assign target ref 
+	//count to object's refcount, destructor should decrement it.
+	template <class C_>
+	class RefCounter {
+	  template<class C2_> friend class RefCounter;
+	protected:
+		RefCounter() : refcnt(new int(1)) {	
 		}
 
-		refcnt=ref.refcnt;
-		addref();
-	}
+		template <class C2_>
+		RefCounter(const RefCounter<C2_> &ref) : refcnt(ref.refcnt) {
+		}
+
+		RefCounter(const RefCounter &ref) : refcnt(ref.refcnt) {
+		}
+
+		void refassign(const RefCounter &ref) {
+			removeref();
+
+			if(!refcheck()) {
+				static_cast<C_*>(this)->destroy();
+				delete refcnt;
+			}
+
+			refcnt=ref.refcnt;
+			addref();
+		}
 	
-	bool refcheck() const {
-		return (*refcnt)>0;
-	}
-
-	void addref() const {
-		(*refcnt)++;
-	}
-
-	void destructref() {
-		removeref();
-
-		if(!refcheck()) {
-			static_cast<C_*>(this)->destroy();
-			delete refcnt;
+		bool refcheck() const {
+			return (*refcnt)>0;
 		}
-	}
 
-	void removeref() {
-		if(*refcnt>0)
-			(*refcnt)--;
-
-		if(*refcnt==0) {
-			static_cast<C_*>(this)->dealloc();
+		void addref() const {
+			(*refcnt)++;
 		}
-	}
 
-	void removeref() const {
-		if(*refcnt>0)
-			(*refcnt)--;
+		void destructref() {
+			removeref();
 
-		if(*refcnt==0) {
-			throw std::runtime_error("Cannot destroy a const object");
+			if(!refcheck()) {
+				static_cast<C_*>(this)->destroy();
+				delete refcnt;
+			}
 		}
-	}
 
-	int getrefcount() const {
-		return *refcnt;
-	}
+		void removeref() {
+			if(*refcnt>0)
+				(*refcnt)--;
 
-	////Reference count
-	mutable int *refcnt;
-};
+			if(*refcnt==0) {
+				static_cast<C_*>(this)->dealloc();
+			}
+		}
+
+		void removeref() const {
+			if(*refcnt>0)
+				(*refcnt)--;
+
+			if(*refcnt==0) {
+				throw std::runtime_error("Cannot destroy a const object");
+			}
+		}
+
+		int getrefcount() const {
+			return *refcnt;
+		}
+
+		////Reference count
+		mutable int *refcnt;
+	};
+
+} }
