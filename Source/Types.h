09dd57291e2291392c71ff1ac833975f93cabc6c
cemkalyoncu <cemkalyoncu@gmail.com>
2014-03-26 17:55:39 +0200

    * Missing files

---
+++ Source/Types.h
@@ -0,0 +1,16 @@
+///@file Types.h contains type definitions for Gorgon system
+
+namespace Gorgon {
+		
+	/// Represents smallest cell in memory.
+	typedef unsigned char Byte;
+	
+#ifdef GORGON_USE_DOUBLE
+	typedef double Float;
+#else
+	/// Represents floating point data type. GORGON_USE_DOUBLE definition
+	/// allows switching to double for precision sensitive functions.
+	typedef float Float;
+#endif
+	
+}
