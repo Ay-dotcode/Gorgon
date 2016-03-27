41d2e0565b977f70a1b326c03b8e2d052dee5ed6
cemkalyoncu <cemkalyoncu@gmail.com>
2016-03-27 07:49:19 +0300

    * Design: Supportive modules are finished (for now)

--- Source/Gorgon/Encoding/URI.cpp
+++ Source/Gorgon/Encoding/URI.cpp
@@ -7,7 +7,7 @@
 #include <string>
 #include "URI.h"
 
-namespace gge { namespace encoding { 
+namespace Gorgon { namespace Encoding { 
 	const char HEX2DEC[256] = 
 	{
 		/*       0  1  2  3   4  5  6  7   8  9  A  B   C  D  E  F */
