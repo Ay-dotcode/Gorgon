2ac2533349ea79acfbb83ac8334de3b7751c1157
cemkalyoncu <cemkalyoncu@gmail.com>
2017-06-19 09:39:33 +0300

    * Additional example code in generic test

--- Testing/Source/Manual/Generic.cpp
+++ Testing/Source/Manual/Generic.cpp
@@ -17,10 +17,27 @@ int main() {
 	Graphics::Layer l;
     app.wind.Add(l);
 
+    auto obj1 = Circle(15);
+    obj1.Prepare();
 
+    auto obj2 = Rectangle(30,30);
+    obj2.Prepare();
+
+    Graphics::BitmapAnimationProvider anim;
+    anim.Add(obj1, 500);
+    anim.Add(obj2, 500);
+    auto &a = anim.CreateAnimation();
+    
+    Geometry::Pointf p(0,0);
 
 
 	while(true) {
+        p+={1,0};
+        
+        l.Clear();
+        
+        a.Draw(l, p, 0xff000080);
+
 		Gorgon::NextFrame();
 	}
 
