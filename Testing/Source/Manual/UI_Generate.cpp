78724ee1462241dc04fd7645b2db5f16ed581952
Yakup Beyoglu <yakupbeyoglu@gmail.com>
2019-07-28 20:53:22 +0300

    Button tutorial (change color hover, down,regular)

--- Testing/Source/Manual/UI_Generate.cpp
+++ Testing/Source/Manual/UI_Generate.cpp
@@ -16,18 +16,18 @@ using namespace Gorgon;
 
 int main() {
 	basic_Application<UI::Window> app("uitest", "UI Generator Test", helptext, 1, 0x40);
-
 	Graphics::Layer l;
-    app.wind.Add(l);
-
+	app.wind.Add(l);
 	Widgets::SimpleGenerator gen;
-
+	gen.Forecolor.Regular.Blend(Gorgon::Graphics::Color::AquaBlue);
+	gen.Forecolor.Down.Blend(Gorgon::Graphics::Color::AquaGreen);
+	gen.Forecolor.Hover.Blend(Gorgon::Graphics::Color::Red);
 	auto temp = gen.Button();
-
 	Widgets::Button btn(temp, "Helloo", []() { std::cout<<"Hello..."<<std::endl; });
 
 	app.wind.Add(btn);
-
+	btn.Move({100,100});
+	
     app.wind.Run();
 
 	return 0;
