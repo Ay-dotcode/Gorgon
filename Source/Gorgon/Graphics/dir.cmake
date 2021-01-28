1f0a12482fcadf6e2317b65de43fa36368d5644d
cemkalyoncu <cemkalyoncu@gmail.com>
2021-01-28 09:05:38 +0200

    * FreeType builtin compilation

--- Source/Gorgon/Graphics/dir.cmake
+++ Source/Gorgon/Graphics/dir.cmake
@@ -33,7 +33,11 @@ SET(Local
 	HTMLRenderer.cpp
 )
 
-if(${FREETYPE})
+if(FREETYPE STREQUAL BUILTIN)
+    list(APPEND Local FreeType.h FreeType.cpp)
+endif()
+
+if(FREETYPE STREQUAL SYSTEM)
     list(APPEND Local FreeType.h FreeType.cpp)
 
 	find_package(Freetype REQUIRED)
@@ -41,7 +45,6 @@ if(${FREETYPE})
 	list(APPEND Libs ${FREETYPE_LIBRARIES})   
 	
 	include_directories(${FREETYPE_INCLUDE_DIRS})
-	
 endif()
 
 EmbedShaders(ShaderSrc.strings.gen Shaders.cpp 
