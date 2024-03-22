7e310fb7c0e6a0e78d48a46e2cc29fdd198d5828
Abdüssamet ERSOYLU <abdussametersoylu@gmail.com>
2024-03-22 23:06:06 +0200

    removed old code removed old code and created game namespace under gorgon source. working on parser now

--- map-dev/MapLoader/Source/MapLoader/MapLoader.cpp
+++
@@ -1,6 +0,0 @@
-#include "MapLoader.h"
-#include <Gorgon/Graphics/Bitmap.h>
-#include <Gorgon/Graphics/Color.h>
-#include <Gorgon/UI/Dimension.h>
-#include <Gorgon/UI/Window.h>
-
--- map-dev/MapLoader/Source/Matrix/MatrixLiterals.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-
-template <typename T>
-using matrix_t = std::vector<std::vector<T>>; 
-template <typename T>
-using matrix_column_t = std::vector<T>;
